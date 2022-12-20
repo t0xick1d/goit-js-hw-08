41632c925be3715671bffee0fba62a10c4b4d8be
zheniaStukalo <82517499+t0xick1d@users.noreply.github.com>
2022-12-20 21:09:38 +0200

    compite 3task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
-console.log('sdasdas');
+import vimeoPlayer from '@vimeo/player';
+const throttle = require('lodash.throttle');
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+console.log('player', player);
+
+// player.on('play', function () {
+//   console.log('played the video!');
+// });
+
+// player.getVideoTitle().then(function (title) {
+//   console.log('title:', title);
+// });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+const throttle = require('lodash.throttle');
 
+const emailUrl = document.querySelector('input');
+const messageUrl = document.querySelector('textarea');
+const formUrl = document.querySelector('.feedback-form');
+
+const DATA_FORM_KEY = 'feedback-form-state';
+
+emailUrl.addEventListener('input', throttle(onChangeInput, 500));
+messageUrl.addEventListener('input', throttle(onChangeInput, 500));
+formUrl.addEventListener('submit', onSubmitForm);
+
+const formData = {};
+
+function onChangeInput(e) {
+  const inputValue = e.target.value;
+  const inputName = e.target.name;
+  formData[inputName] = inputValue;
+  localStorage.setItem(DATA_FORM_KEY, JSON.stringify(formData));
+}
+setCurrentValueForm();
+
+function setCurrentValueForm() {
+  const setActualValue = JSON.parse(localStorage.getItem(DATA_FORM_KEY));
+
+  console.log(setActualValue);
+
+  setActualValue.email ? (emailUrl.value = setActualValue.email) : '';
+
+  setActualValue.message ? (messageUrl.value = setActualValue.message) : '';
+}
+
+function onSubmitForm(e) {
+  const email = JSON.parse(localStorage.getItem(DATA_FORM_KEY)).email;
+  const message = JSON.parse(localStorage.getItem(DATA_FORM_KEY)).message;
+  console.log('email :', email);
+  console.log('message:', message);
+  e.preventDefault();
+  localStorage.clear();
+  emailUrl.value = '';
+  messageUrl.value = '';
+}
